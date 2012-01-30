2f40d31318f9bb548c23d2a9fe80602fc4d32321
Johannes Kulick <kulick@hildensia.de>
2012-01-30 14:27:18 +0100

    autolink and clone submodules

---
+++ linkconfig.sh
@@ -0,0 +1,16 @@
+linkfiles=(X11/Xauthority X11/xinitrc X11/Xresources zsh zsh/zshrc zsh/zshenv vim vim/vimrc mpd mpd/mpdconf gitconfig mozilla)
+link_file() {
+  ORG="$HOME/.`basename $1`" 
+  if [ -f $ORG ]
+  then 
+    rm $ORG
+    echo "remove $ORG";
+  fi
+  ln -s ".config/$1" $ORG
+   
+}
+for file in "${linkfiles[@]}"; do
+  link_file $file 
+done
+
+git submodule update --init
