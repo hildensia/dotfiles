297ceefbbad901273165cd6007800d33e963e98e
Johannes Kulick <kulick@hildensia.de>
2012-01-30 14:24:03 +0100

    added locking shortcut

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -291,7 +291,8 @@ clientkeys = awful.util.table.join(
     awful.key({}, "XF86AudioPlay", function() awful.util.spawn("mpc toggle") end),
     awful.key({}, "XF86AudioStop", function() awful.util.spawn("mpc stop") end),
     awful.key({}, "XF86AudioPrev", function() awful.util.spawn("mpc prev") end),
-    awful.key({}, "XF86AudioNext", function() awful.util.spawn("mpc next") end)
+    awful.key({}, "XF86AudioNext", function() awful.util.spawn("mpc next") end),
+    awful.key({}, "XF86ScreenSaver", function() awful.util.spawn("xlock") end)
 
 )
 
