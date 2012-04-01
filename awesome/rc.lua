325076dcb10b086e63da459b2013bddee795ec7c
Johannes Kulick <kulick@hildensia.de>
2012-04-01 12:47:59 +0200

    spawn uzbl on C-S-<CR>

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -240,6 +240,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ modkey, "Shift"   }, "Return", function () awful.util.spawn("uzbl-browser") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
