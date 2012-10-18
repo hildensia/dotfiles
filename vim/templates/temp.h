cd35dfb48026a71bee7aec2d4833bc60caaa487b
Johannes Kulick <kulick@hildensia.de>
2012-10-18 12:35:01 +0200

    add header template

---
+++ vim/templates/temp.h
@@ -0,0 +1,5 @@
+#ifndef _HEADER_GUARD_[:VE:]toupper(expand('%:t:r'))[:EVE:]_H_
+#ifndef _HEADER_GUARD_[:VE:]toupper(expand('%:t:r'))[:EVE:]_H_
+
+#endif // _HEADER_GUARD_[:VE:]toupper(expand('%:t:r'))[:EVE:]_H_
+
