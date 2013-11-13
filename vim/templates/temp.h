6d4f83a6b01b4d7027797e6e789642289d0d577c
Johannes Kulick <kulick@hildensia.de>
2013-11-13 10:55:30 +0100

    old vim stuff

--- vim/templates/temp.h
+++
@@ -1,5 +0,0 @@
-#ifndef _HEADER_GUARD_[:VE:]toupper(expand('%:t:r'))[:EVE:]_H_
-#ifndef _HEADER_GUARD_[:VE:]toupper(expand('%:t:r'))[:EVE:]_H_
-
-#endif // _HEADER_GUARD_[:VE:]toupper(expand('%:t:r'))[:EVE:]_H_
-
