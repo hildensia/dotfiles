363b5d7957b25c2c8f8a95f3806804a23298e517
Johannes Kulick <kulick@hildensia.de>
2014-03-26 12:06:44 +0100

    install bundles right with linkconfig.sh

--- linkconfig.sh
+++ linkconfig.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 linkfiles=(X11/Xauthority X11/xinitrc X11/Xresources zsh zsh/zshrc zsh/zshenv vim vim/vimrc mpd mpd/mpdconf gitconfig gdb/gdbinit tmux.conf)
 link_file() {
   ORG="$HOME/.`basename $1`" 
@@ -13,5 +15,6 @@ for file in "${linkfiles[@]}"; do
   link_file $file 
 done
 
-git submodule update --init
+git submodule update --init  # get vundle
+vim +BundleInstall +qall  # install all bundles via vundle
 mkdir vim/undo # don't track the undo files, but create a dir for them
